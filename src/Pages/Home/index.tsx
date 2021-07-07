a8cb7c426d72e6d8086cfd2fed91524d71f966bc
koch21 <hagnakoch@gmail.com>
2021-07-07 04:04:31 -0300

    Primeiro commit, Inicio do Projeto

---
+++ src/Pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, HomeTxt } from './styles';
+
+const Home: React.FC = () => {
+  return (
+    <Container>
+      <HomeTxt>Hello World!</HomeTxt>
+    </Container>
+  )
+}
+
+export default Home;
